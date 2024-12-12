dc0e2061180282cc81e98bf14b9e51f5dc271e4e
flj <sustdx1988@gmail.com>
2024-12-12 10:41:17 +0800

    optimize avx512

--- mat/matrix.cc
+++ mat/matrix.cc
@@ -19,7 +19,7 @@ void mat_init(float m[N][N])
             m[i][j] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 }
 
-void matmul_1(float out[N][N], const float A[N][N],const float B[N][N])
+void matmul_base(float out[N][N], const float A[N][N],const float B[N][N])
 {
     size_t mat_size = N*N*sizeof(float);
     memset(out,0,mat_size);
@@ -31,7 +31,7 @@ void matmul_1(float out[N][N], const float A[N][N],const float B[N][N])
                 out[i][j] += A[i][k]*B[k][j];
 }
 
-void matmul_2(float out[N][N], const float A[N][N],const float B[N][N])
+void matmul_cacheline(float out[N][N], const float A[N][N],const float B[N][N])
 {
     size_t mat_size = N*N*sizeof(float);
     memset(out,0,mat_size);
@@ -48,20 +48,25 @@ void matmul_avx512(float out[N][N], const float A[N][N],const float B[N][N])
 {
     const int BLOCK_SIZE= 16;
     size_t mat_size = N*N*sizeof(float);
-    memset(out,0,mat_size);
+    memset(out,0,mat_size);    
 
     #pragma omp parallel for
     for(int i=0;i<N;i++)
-        for(int j=0;j<N;j++)
-        {
-            __m512 a = _mm512_set1_ps(A[i][j]);
+    {
+        for(int j=0;j<N;j+=BLOCK_SIZE){
+            __m512 a = _mm512_load_ps(&A[i][j]);
             for(int k=0;k<N;k+=BLOCK_SIZE){
-                __m512 b = _mm512_load_ps(&B[j][k]);
-                __m512 c = _mm512_load_ps(&out[i][k]);
-                __m512 r = _mm512_mul_ps(a,b);
-                _mm512_store_ps(&out[i][k],_mm512_add_ps(c,r));
+                __m512 o = _mm512_load_ps(&out[i][k]);;
+                for(int l=0;l<BLOCK_SIZE;l++)
+                {
+                    __m512 c = _mm512_set1_ps(a[l]);
+                    __m512 b = _mm512_load_ps(&B[j+l][k]);
+                    o = _mm512_fmadd_ps(c,b,o);
+                }
+                _mm512_store_ps(&out[i][k],o);
             }
         }
+    }
 }
 #endif
 
@@ -81,26 +86,29 @@ alignas(64) float C1[N][N],C2[N][N],C3[N][N];
 
 int main(int argc,char* argv[])
 {
+    const int LOOP_CNT = 50;
     mat_init(A);
     mat_init(B);
 
 
     auto start = get_current_time_ms();
-    matmul_2(C2,A,B);
-    printf("matmul_2 = %ld ms\n",get_current_time_ms()-start);
-
+    for(int i=0;i<LOOP_CNT;i++)
+        matmul_base(C1,A,B);
+    printf("matmul_base = %0.2f ms\n",(float)(get_current_time_ms()-start)/LOOP_CNT);
 
     start = get_current_time_ms();
-    matmul_1(C1,A,B);
-    printf("matmul_1 = %ld ms\n",get_current_time_ms()-start);
+    for(int i=0;i<LOOP_CNT;i++)
+        matmul_cacheline(C2,A,B);
+    printf("matmul_cacheline = %0.2f ms\n",(float)(get_current_time_ms()-start)/LOOP_CNT);
 
     compare_mat(C1,C2);
 
 #if AVX512F_CHECK
     start = get_current_time_ms();
-    matmul_avx512(C3,A,B);
-    printf("matmul_avx512 = %ld ms\n",get_current_time_ms()-start);
-    compare_mat(C1,C3);
+    for(int i=0;i<LOOP_CNT;i++)
+        matmul_avx512(C3,A,B);
+    printf("matmul_avx512 = %0.2f ms\n",(float)(get_current_time_ms()-start)/LOOP_CNT);
+    compare_mat(C2,C3);
 #endif
 
     return 0;
--- mat/matrix_torch.py
+++ mat/matrix_torch.py
@@ -1,13 +1,13 @@
 import torch
 import time
 
+N = 1024
+a = torch.randn(N,N)
+b = torch.randn(N,N)
 
-a = torch.randn(1024,1024)
-b = torch.randn(1024,1024)
-
-for i in range(10):
-    a.matmul(b)
+LOOP_CNT = 50
 
 start = time.perf_counter()
-c = a.matmul(b)
-print(f"torch cpu {int((time.perf_counter()-start)*1000)} ms")
+for i in range(LOOP_CNT):
+    c = a.matmul(b)
+print(f"torch cpu {(time.perf_counter()-start)*1000/LOOP_CNT:0.2f} ms")
